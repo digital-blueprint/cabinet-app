4e04da9a9ca9fcc67eb4a87f1d9f21cae72eac29
Shiva Pouya <shiva.pouya@tugraz.at>
2024-09-26 12:15:42 +0200

    Updated com doc hit box style

--- src/objectTypes/fileCommunication.js
+++ src/objectTypes/fileCommunication.js
@@ -60,16 +60,43 @@ class CabinetHitElement extends BaseHitElement {
             h2 {
                 color: #1ace38;
             }
+            .ais-com-Hits-header{
+                border-bottom: 1px solid rgb(34, 33, 32);
+                margin-bottom: calc(7px + 1vh);
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+                gap: 10px;
+                padding: 5px;
+            }
+            .ais-com-Hits-right-column {
+                display: flex;
+                justify-content: flex-end;
+
+            }
+            .ais-com-Hits-content {
+                margin-bottom:calc(150px + 5vh);
+            }    
         `;
     }
 
     render() {
         return html`
             <form>
+                <header class="ais-com-Hits-header">
                 <h2>Communication</h2>
+                <div class="ais-com-Hits-right-column">
+                <svg width="1.4em" xmlns="http://www.w3.org/2000/svg" height="1.4em" viewBox="1714.544 1153.736 30.853 30.496" fill="none"><path d="M1744.208 1153.736h-15.296a1.25 1.25 0 0 0-1.248 1.248v5.792h-11.872a1.25 1.25 0 0 0-1.248 1.248v14.24c0 .096.032.16.096.224l7.68 7.648a.29.29 0 0 0 .224.096h8.448a1.25 1.25 0 0 0 1.248-1.248v-9.056l3.2 3.168a.29.29 0 0 0 .224.096h8.512c.672 0 1.216-.544 1.216-1.216v-20.992c.064-.672-.512-1.248-1.184-1.248Zm-22.272 27.52-4.352-4.352h4.352v4.352Zm8.544 1.248h-6.784v-6.112c0-.672-.576-1.216-1.248-1.216h-6.112v-12.64h14.176l-.032 19.968Zm1.76-12.608h2.784v4.288l-2.784-2.72v-1.568Zm11.456 5.504h-6.88v-6.016c0-.672-.544-1.248-1.216-1.248h-3.36v-6.112c0-.672-.576-1.248-1.216-1.248h-1.6v-5.28h14.24l.032 19.904Z" style="fill: rgb(158, 30, 77); fill-opacity: 1;" class="fills" data-testid="svg-path"/></svg>
+                </div>
+                <h2>Communication</h2>
+                <h2>Communication</h2>
+                <h2>Communication</h2>
+                <h2>Communication</h2>
+                </header>
+                <main class="ais-com-Hits-content">
                 Some special information here<br />
                 lang: ${this.lang}<br />
                 filename: ${this.data.file.base.fileName}<br />
+                </main>
         `;
     }
 }
@@ -77,6 +104,7 @@ class CabinetHitElement extends BaseHitElement {
 class CabinetViewElement extends BaseViewElement {
     render() {
         return html`
+            <h2>Communication</h2>
             <h2>Communication</h2>
             lang: ${this.lang}<br />
             filename: ${this.data.file.base.fileName}<br />
