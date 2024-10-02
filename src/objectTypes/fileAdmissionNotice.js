c0ac84d8c8bf19012ec4c8798462a79ed6a98b4c
Shiva Pouya <shiva.pouya@tugraz.at>
2024-10-02 09:33:44 +0200

    Added admission hit box style

--- src/objectTypes/fileAdmissionNotice.js
+++ src/objectTypes/fileAdmissionNotice.js
@@ -42,8 +42,8 @@ class CabinetFormElement extends BaseFormElement {
         console.log('this.data', this.data);
         // const data = this.data;
 
-        // Schema:  https://gitlab.tugraz.at/dbp/middleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/admissionNotice.schema.json
-        // Example: https://gitlab.tugraz.at/dbp/middleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/examples/admissionNotice_example.json
+        // Schema:  https://gitlab.tugraz.at/dbp/madmissiondleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/admissionNotice.schema.json
+        // Example: https://gitlab.tugraz.at/dbp/madmissiondleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/examples/admissionNotice_example.json
         return html`
             <form>
                 <h2>admissionNotice Form</h2>
@@ -67,19 +67,78 @@ class CabinetHitElement extends BaseHitElement {
         return css`
             ${super.styles}
 
-            h2 {
-                color: #2baff5;
+            
+            .ais-admission-Hits-header{
+                border-bottom: 1px solid rgb(34, 33, 32);
+                margin-bottom: calc(7px + 1vh);
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+                gap: 10px;
+                padding: 5px;
             }
+            .ais-admission-Hits-header-items{
+                display: flex;
+            }
+            .ais-admission-Hits-content {
+                display: grid;
+                grid-template-rows: repeat(3, 1fr);
+                gap: 10px;
+            }
+            .hit-content-item1 {
+                grid-row: 1/2; 
+            }     
+            .hit-content-item2 {
+                grid-row: 2/3; 
+            }     
+            .hit-content-item3 {
+                grid-row: 3/3;
+                padding-top: 30px;  
+            }
+            .admission-item1 {
+                grid-column: 1 / 2;
+                color:#2baff5;
+            }
+            .admission-item2 {
+                grid-column: 3 / 4;
+                justify-self: end; 
+            }
+            .admission-item3 {
+                grid-column: 1 / 2;
+                 
+            }
+            .admission-item4 {
+                grid-column: 2 / 3;
+                
+            }
+            .admission-item5 {
+                grid-column: 3 / 4;
+                
+            }     
         `;
     }
 
     render() {
         return html`
             <form>
-                <h2>admissionNotice</h2>
+                <header class="ais-admission-Hits-header">
+                <div class="ais-admission-Hits-header-items admission-item1">admissionNotice(Family name,given name)</div>
+                <div class="ais-admission-Hits-header-items admission-item2">
+                <svg width="1.4em" xmlns="http://www.w3.org/2000/svg" height="1.4em" viewBox="1714.544 1153.736 30.853 30.496" fill="none"><path d="M1744.208 1153.736h-15.296a1.25 1.25 0 0 0-1.248 1.248v5.792h-11.872a1.25 1.25 0 0 0-1.248 1.248v14.24c0 .096.032.16.096.224l7.68 7.648a.29.29 0 0 0 .224.096h8.448a1.25 1.25 0 0 0 1.248-1.248v-9.056l3.2 3.168a.29.29 0 0 0 .224.096h8.512c.672 0 1.216-.544 1.216-1.216v-20.992c.064-.672-.512-1.248-1.184-1.248Zm-22.272 27.52-4.352-4.352h4.352v4.352Zm8.544 1.248h-6.784v-6.112c0-.672-.576-1.216-1.248-1.216h-6.112v-12.64h14.176l-.032 19.968Zm1.76-12.608h2.784v4.288l-2.784-2.72v-1.568Zm11.456 5.504h-6.88v-6.016c0-.672-.544-1.248-1.216-1.248h-3.36v-6.112c0-.672-.576-1.248-1.216-1.248h-1.6v-5.28h14.24l.032 19.904Z" style="fill: rgb(158, 30, 77); fill-opacity: 1;" class="fills" data-testadmission="svg-path"/></svg>
+                </div>
+                <div class="ais-admission-Hits-header-items admission-item3">birthDate</div>
+                <div class="ais-admission-Hits-header-items admission-item4">studId</div>
+                <div class="ais-admission-Hits-header-items admission-item5">stPersonNr</div>
+                </header>
+                <main class="ais-admission-Hits-content">
+                <header class="ais-admission-Hits-content-items hit-content-item1">documentType: ${this.data.additionalType}</header>
                 lang: ${this.lang}<br />
                 <!-- filename: ${this.data.file.base.fileName}<br /> -->
-                filename: ${renderFieldWithHighlight(this.data, 'file.base.fileName')}<br />
+                <div class="ais-admission-Hits-content-items hit-content-item2">filename: ${renderFieldWithHighlight(this.data, 'file.base.fileName')}</div>
+                <div class="ais-admission-Hits-content-items hit-content-item3">
+                dateCreated: <br/>
+                lastModified: 
+                </div>
+                </main>
         `;
     }
 }
