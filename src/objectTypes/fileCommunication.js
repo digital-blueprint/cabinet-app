d205556ef051c995979f40a01b661f4c68177cdd
Shiva Pouya <shiva.pouya@tugraz.at>
2024-10-28 09:02:13 +0100

    Added common name for classes in doc hit box

--- src/objectTypes/fileCommunication.js
+++ src/objectTypes/fileCommunication.js
@@ -62,7 +62,7 @@ class CabinetHitElement extends BaseHitElement {
         return css`
             ${super.styles}
 
-            .ais-com-Hits-header{
+            .ais-doc-Hits-header{
                 border-bottom: 1px solid rgb(34, 33, 32);
                 margin-bottom: calc(7px + 1vh);
                 display: grid;
@@ -70,10 +70,10 @@ class CabinetHitElement extends BaseHitElement {
                 gap: 10px;
                 padding: 5px;
             }
-            .ais-com-Hits-header-items{
+            .ais-doc-Hits-header-items{
                 display: flex;
             }
-            .ais-com-Hits-content {
+            .ais-doc-Hits-content {
                 gap: 10px;
                 display: grid;
                 grid-template-rows: repeat(3, 1fr);
@@ -90,7 +90,7 @@ class CabinetHitElement extends BaseHitElement {
             }
             .header-item1 {
                 grid-column: 1 / 2;
-                color:#1ace38;
+                color:green;
             }
             .header-item2 {
                 grid-column: 3 / 4;
@@ -117,21 +117,22 @@ class CabinetHitElement extends BaseHitElement {
         let hit = /** @type {PersonHit} */(this.data);
         return html`
             <form>
-                <header class="ais-com-Hits-header">
-                <div class="ais-com-Hits-header-items header-item1">${hit.person.fullName}</div>
-                <div class="ais-com-Hits-header-items header-item2">
+                <header class="ais-doc-Hits-header">
+                <div class="ais-doc-Hits-header-items header-item1">${hit.person.fullName}</div>
+                <div class="ais-doc-Hits-header-items header-item2">
                 <svg width="1.4em" xmlns="http://www.w3.org/2000/svg" height="1.4em" viewBox="1714.544 1153.736 30.853 30.496" fill="none"><path d="M1744.208 1153.736h-15.296a1.25 1.25 0 0 0-1.248 1.248v5.792h-11.872a1.25 1.25 0 0 0-1.248 1.248v14.24c0 .096.032.16.096.224l7.68 7.648a.29.29 0 0 0 .224.096h8.448a1.25 1.25 0 0 0 1.248-1.248v-9.056l3.2 3.168a.29.29 0 0 0 .224.096h8.512c.672 0 1.216-.544 1.216-1.216v-20.992c.064-.672-.512-1.248-1.184-1.248Zm-22.272 27.52-4.352-4.352h4.352v4.352Zm8.544 1.248h-6.784v-6.112c0-.672-.576-1.216-1.248-1.216h-6.112v-12.64h14.176l-.032 19.968Zm1.76-12.608h2.784v4.288l-2.784-2.72v-1.568Zm11.456 5.504h-6.88v-6.016c0-.672-.544-1.248-1.216-1.248h-3.36v-6.112c0-.672-.576-1.248-1.216-1.248h-1.6v-5.28h14.24l.032 19.904Z" style="fill: rgb(158, 30, 77); fill-opacity: 1;" class="fills" data-testid="svg-path"/></svg>
                 </div>
-                <div class="ais-com-Hits-header-items header-item3">${hit.person.birthDate}</div>
-                <div class="ais-com-Hits-header-items header-item4">${hit.person.studId}</div>
-                <div class="ais-com-Hits-header-items header-item5">${hit.person.stPersonNr}</div>
+                <div class="ais-doc-Hits-header-items header-item3">${hit.person.birthDate}</div>
+                <div class="ais-doc-Hits-header-items header-item4">${hit.person.studId}</div>
+                <div class="ais-doc-Hits-header-items header-item5">${hit.person.stPersonNr}</div>
                 </header>
-                <main class="ais-com-Hits-content">
+                <main class="ais-doc-Hits-content">
                 lang: ${this.lang}<br />
-                <div class="ais-com-Hits-content-items hit-content-item1">document Type</div>
-                <div class="ais-com-Hits-content-items hit-content-item2">filename: ${this.data.file.base.fileName}</div>
-                <div class="ais-com-Hits-content-items hit-content-item3">
+                <div class="hit-content-item1">document Type</div>
+                <div class="hit-content-item2">filename: ${this.data.file.base.fileName}</div>
+                <div class="hit-content-item3">
                 dateCreated: ${this.data.file['file-cabinet-communication'].dateCreated}<br />
+                Added: <br />
                 lastModified: ${lastModified}<br />
                 </div>
                 </main>
