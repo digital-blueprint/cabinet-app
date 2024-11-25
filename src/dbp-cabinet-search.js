e2e3b27575874d5a86a6377de33c38e1fe1929b4
Shiva Pouya <shiva.pouya@tugraz.at>
2024-11-25 11:36:28 +0100

    file: add placeholder for search box & title for sorting in search box

--- src/dbp-cabinet-search.js
+++ src/dbp-cabinet-search.js
@@ -502,6 +502,7 @@ class CabinetSearch extends ScopedElementsMixin(DBPCabinetLitElement) {
         return searchBox({
             container: this._("#searchbox"),
             showLoadingIndicator: false,
+            placeholder: "Search cabinet",
         });
     }
 
@@ -559,7 +560,7 @@ class CabinetSearch extends ScopedElementsMixin(DBPCabinetLitElement) {
         return sortBy({
             container: this._('#sort-by'),
             items: [
-                { label: 'Best Match', value: `${this.typesenseCollection}` }, /* default sorting "@type:desc,_text_match:desc,person.familyName:asc" */
+                { label: 'sort by : default', value: `${this.typesenseCollection}` }, /* default sorting "@type:desc,_text_match:desc,person.familyName:asc" */
                 { label: 'Family name', value: `${this.typesenseCollection}/sort/@type:desc,person.familyName:asc,_text_match:desc` },
                 { label: 'Last modified Documents', value: `${this.typesenseCollection}/sort/@type:asc,file.base.modifiedTimestamp:desc,_text_match:desc` }
             ],
