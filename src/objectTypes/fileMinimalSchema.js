44ad763f2ee0623a01cc0f36bdf1bc27a1cf125c
Shiva Pouya <shiva.pouya@tugraz.at>
2024-11-27 15:27:04 +0100

    file: add value for doc issue date for minimal schema object type

--- src/objectTypes/fileMinimalSchema.js
+++ src/objectTypes/fileMinimalSchema.js
@@ -81,7 +81,7 @@ class CabinetHitElement extends BaseHitElement {
                 <header class="ais-doc-Hits-content-items hit-content-item1">${i18n.t('document-type')}:&nbsp;${this.data.file.base.additionalType.text} </header>
                 <div class="ais-doc-Hits-content-items hit-content-item2"></div>
                 <div class="ais-mdoc-Hits-content-items hit-content-item3">
-                ${i18n.t('document-issue-date')}: <br/>
+                ${i18n.t('document-issue-date')}:${this.data.file['file-cabinet-minimalSchema'].dateCreated} <br/>
                 ${i18n.t('Added')}: <br />
                 ${i18n.t('last-modified')}:${lastModified}<br />
                 </div>
