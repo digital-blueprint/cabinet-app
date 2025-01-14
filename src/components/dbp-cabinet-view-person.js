c2ea1f44bbddd346a0bfe2bf3dbd879b78d5aecd
Shiva Pouya <shiva.pouya@tugraz.at>
2025-01-14 12:00:01 +0100

    file: update person view modal width to make it wider

--- src/components/dbp-cabinet-view-person.js
+++ src/components/dbp-cabinet-view-person.js
@@ -102,6 +102,7 @@ export class CabinetViewPerson extends ScopedElementsMixin(DBPCabinetLitElement)
                 --dbp-modal-title-padding: 0 0 0 40px;
                 --dbp-modal-title-background: url("${unsafeCSS(getIconSVGURL('user'))}") left center / 28px 28px no-repeat;
                 list-style-type: none;
+                width: var(--dbp-modal-max-width,60vw);
             }
         `;
     }
