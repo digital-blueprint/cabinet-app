d143b4daa969f65943eb34e8b7a5410f6da091b6
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-25 11:20:48 +0100

    git: also check for whitespaces
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -6,7 +6,7 @@ echo "Running pre-commit hook with npm run format"
 npm run format
 
 # Check if there are any changes after formatting
-if ! git diff --exit-code; then
+if ! git diff --check --exit-code; then
   echo -e "\nCode may have been formatted. Please review the changes and commit again."
   exit 1
 fi
