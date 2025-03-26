cb6043acc669f0c7e8f2ac6131bff79c8d0ea970
Shiva Pouya <shiva.pouya@tugraz.at>
2025-03-26 15:12:03 +0100

    file: add time to the communication object type in doc view modal

--- src/objectTypes/fileCommunication.js
+++ src/objectTypes/fileCommunication.js
@@ -213,12 +213,12 @@ class CabinetViewElement extends BaseViewElement {
                 label=${this._i18n.t('communication-abstract')}
                 .value=${communication.abstract || ''}></dbp-form-string-view>
 
-            <dbp-form-date-view
+            <dbp-form-datetime-view
                 subscribe="lang"
                 label=${this._i18n.t('doc-modal-issue-date')}
                 .value=${communication.dateCreated
                     ? new Date(communication.dateCreated)
-                    : ''}></dbp-form-date-view>
+                    : ''}></dbp-form-datetime-view>
         `;
     }
 }
