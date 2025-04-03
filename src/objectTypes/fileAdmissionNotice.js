36760aeacb9bab51d2e2a336ea73f355ff68e721
Shiva Pouya <shiva.pouya@tugraz.at>
2025-04-03 14:37:22 +0200

    file: add formatDate function to admission object type

--- src/objectTypes/fileAdmissionNotice.js
+++ src/objectTypes/fileAdmissionNotice.js
@@ -7,6 +7,7 @@ import {
     getCommonStyles,
 } from '../baseObject.js';
 import {getDocumentHit, getAdmissionNotice} from './schema.js';
+import {formatDate} from '../utils.js';
 
 export default class extends BaseObject {
     name = 'file-cabinet-admissionNotice';
@@ -140,11 +141,7 @@ class CabinetHitElement extends BaseHitElement {
                         </div>
                         &nbsp
                         <div class="ais-doc-Hits-header-items header-item2">
-                            ${Intl.DateTimeFormat('de', {
-                                year: 'numeric',
-                                month: '2-digit',
-                                day: '2-digit',
-                            }).format(new Date(hit.person.birthDate))}&nbsp(${hit.person
+                            ${formatDate(hit.person.birthDate)}&nbsp(${hit.person
                                 .studId}&nbsp|&nbsp${hit.person.stPersonNr})
                         </div>
                     </div>
