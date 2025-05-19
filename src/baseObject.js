digital-blueprint/cabinet-app

commit 9e9684f8ed36a614768bb2aa8cd1aadea0a798ba

Fix file upload initial state rendering
this.data is not a hit in this case, but an empty object.
Replace with a default hit so we can use the same JS types for
the rendering.

I tried making "data" nullable itself, but there is too much code
depending on it being an object right now.

src/baseObject.js
@@ -5,6 +5,7 @@ import '@dbp-toolkit/form-elements';
 import {createInstance} from './i18n';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import * as formElements from './objectTypes/formElements';
+import {getDocumentHit} from './objectTypes/schema.js';
 import {classMap} from 'lit/directives/class-map.js';
 import {
     gatherFormDataFromElement,
@@ -88,6 +89,39 @@ export const getCommonStyles = () => css`
     }
 `;
 
+function getDefaultSemester() {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    currentYear = currentYear % 100;
+    let currentMonth = currentDate.getMonth();
+    let currentSeason;
+    if (currentMonth >= 2 && currentMonth <= 8) {
+        currentSeason = 'S';
+    } else {
+        currentSeason = 'W';
+    }
+    return currentYear.toString() + currentSeason;
+}
+
+const DEFAULT_FILE_COMMON = {
+    '@type': 'DocumentFile',
+    file: {
+        base: {
+            additionalType: {
+                key: '',
+                text: '',
+            },
+            groupId: null,
+            comment: null,
+            isPartOf: [],
+            studyField: '',
+            studyFieldName: '',
+            subjectOf: null,
+            semester: getDefaultSemester(),
+        },
+    },
+};
+
 export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
     constructor() {
         super();
@@ -99,6 +133,15 @@ export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
         this.entryPointUrl = '';
         this.auth = {};
         this.saveButtonEnabled = true;
+    }
+
+    _getData() {
+        // Return the hit if set, otherwise null
+        // FIXME: make this.data nullable
+        if (Object.keys(this.data).length === 0) {
+            return null;
+        }
+        return this.data;
     }
 
     enableSaveButton() {
@@ -152,24 +195,22 @@ export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
     };
 
     getCommonFormElements = () => {
-        const fileData = this.data?.file || {};
-        const baseData = fileData.base || {};
-        const defaultSemester = this.getDefaultSemester(baseData);
-        const additionalType = this.additionalType || baseData.additionalType?.key || '';
+        let hit = getDocumentHit(this._getData() ?? DEFAULT_FILE_COMMON);
+        let fileCommon = hit.file.base;
 
         return html`
             <dbp-form-string-element
                 subscribe="lang"
                 name="subjectOf"
                 label=${this._i18n.t('doc-modal-subject-of')}
-                .value=${baseData.subjectOf || ''}></dbp-form-string-element>
+                .value=${fileCommon.subjectOf || ''}></dbp-form-string-element>
 
             <dbp-form-enum-element
                 subscribe="lang"
                 name="studyField"
                 label=${this._i18n.t('doc-modal-study-field')}
                 .items=${this.getStudyFields()}
-                .value=${baseData.studyField || ''}
+                .value=${fileCommon.studyField}
                 required></dbp-form-enum-element>
 
             <dbp-form-enum-element
@@ -177,7 +218,7 @@ export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
                 name="semester"
                 label=${this._i18n.t('doc-modal-semester')}
                 .items=${this.getSemesters()}
-                .value=${defaultSemester}
+                .value=${fileCommon.semester}
                 required></dbp-form-enum-element>
 
             <dbp-form-enum-element
@@ -185,7 +226,7 @@ export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
                 name="isPartOf"
                 label=${this._i18n.t('doc-modal-purpose-storage')}
                 .items=${BaseFormElement.getIsPartOfItems(this._i18n)}
-                .value=${baseData.isPartOf || ''}
+                .value=${fileCommon.isPartOf}
                 multiple
                 required></dbp-form-enum-element>
 
@@ -194,30 +235,12 @@ export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
                 name="comment"
                 label=${this._i18n.t('doc-modal-comment')}
                 rows="5"
-                .value=${baseData.comment || ''}></dbp-form-string-element>
+                .value=${fileCommon.comment || ''}></dbp-form-string-element>
 
-            <input type="hidden" name="additionalType" value="${additionalType}" />
+            <input type="hidden" name="additionalType" value="${fileCommon.additionalType.key}" />
             ${this.getButtonRowHtml()}
         `;
     };
-
-    getDefaultSemester(baseData) {
-        if (baseData.semester) {
-            return baseData.semester;
-        } else {
-            let currentDate = new Date();
-            let currentYear = currentDate.getFullYear();
-            currentYear = currentYear % 100;
-            let currentMonth = currentDate.getMonth();
-            let currentSeason;
-            if (currentMonth >= 2 && currentMonth <= 8) {
-                currentSeason = 'S';
-            } else {
-                currentSeason = 'W';
-            }
-            return currentYear.toString() + currentSeason;
-        }
-    }
 
     async validateForm() {
         const formElement = this.shadowRoot.querySelector('form');

src/objectTypes/fileAdmissionNotice.js
@@ -29,6 +29,18 @@ export default class extends BaseObject {
     }
 }
 
+const DEFAULT_ADMISSION_NOTICE = {
+    '@type': 'DocumentFile',
+    objectType: 'file-cabinet-admissionNotice',
+    file: {
+        'file-cabinet-admissionNotice': {
+            dateCreated: '',
+            previousStudy: '',
+            decision: '',
+        },
+    },
+};
+
 class CabinetFormElement extends BaseFormElement {
     static getAdditionalTypes = () => {
         return {
@@ -46,9 +58,9 @@ class CabinetFormElement extends BaseFormElement {
 
     render() {
         console.log('-- Render CabinetFormElement --');
-        console.log('this.data', this.data);
+        console.log('this.data', this._getData());
 
-        let hit = getDocumentHit(this.data);
+        let hit = getDocumentHit(this._getData() ?? DEFAULT_ADMISSION_NOTICE);
         let admissionNotice = getAdmissionNotice(hit);
 
         // Schema:  https://gitlab.tugraz.at/dbp/middleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/admissionNotice.schema.json
@@ -59,21 +71,21 @@ class CabinetFormElement extends BaseFormElement {
                     subscribe="lang"
                     name="dateCreated"
                     label=${this._i18n.t('doc-modal-issue-date')}
-                    .value=${admissionNotice.dateCreated || ''}
+                    .value=${admissionNotice.dateCreated}
                     required></dbp-form-date-element>
 
                 <dbp-form-string-element
                     subscribe="lang"
                     name="previousStudy"
                     label=${this._i18n.t('doc-modal-previousStudy')}
-                    .value=${admissionNotice.previousStudy || ''}></dbp-form-string-element>
+                    .value=${admissionNotice.previousStudy}></dbp-form-string-element>
 
                 <dbp-form-enum-element
                     subscribe="lang"
                     name="decision"
                     label=${this._i18n.t('doc-modal-decision')}
                     .items=${CabinetFormElement.getDecisions()}
-                    .value=${admissionNotice.decision || ''}></dbp-form-enum-element>
+                    .value=${admissionNotice.decision}></dbp-form-enum-element>
 
                 ${this.getCommonFormElements()}
             </form>

src/objectTypes/fileCitizenshipCertificate.js
@@ -30,6 +30,17 @@ export default class extends BaseObject {
     }
 }
 
+const DEFAULT_CITIZENSHIP_CERTIFICATE = {
+    '@type': 'DocumentFile',
+    objectType: 'file-cabinet-citizenshipCertificate',
+    file: {
+        'file-cabinet-citizenshipCertificate': {
+            nationality: '',
+            dateCreated: '',
+        },
+    },
+};
+
 class CabinetFormElement extends BaseFormElement {
     static getAdditionalTypes = () => {
         return {
@@ -40,7 +51,7 @@ class CabinetFormElement extends BaseFormElement {
     render() {
         console.log('-- Render CabinetFormElement --');
 
-        let hit = getDocumentHit(this.data);
+        let hit = getDocumentHit(this._getData() ?? DEFAULT_CITIZENSHIP_CERTIFICATE);
         let citizenshipCertificate = getCitizenshipCertificate(hit);
 
         // Schema:  https://gitlab.tugraz.at/dbp/middleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/citizenshipCertificate.schema.json
@@ -52,13 +63,13 @@ class CabinetFormElement extends BaseFormElement {
                     name="nationality"
                     label=${this._i18n.t('doc-modal-nationality')}
                     .items=${formElements.getNationalityItems()}
-                    .value=${citizenshipCertificate.nationality || ''}></dbp-form-enum-element>
+                    .value=${citizenshipCertificate.nationality}></dbp-form-enum-element>
 
                 <dbp-form-date-element
                     subscribe="lang"
                     name="dateCreated"
                     label=${this._i18n.t('doc-modal-issue-date')}
-                    .value=${citizenshipCertificate.dateCreated || ''}
+                    .value=${citizenshipCertificate.dateCreated}
                     required></dbp-form-date-element>
 
                 ${this.getCommonFormElements()}

src/objectTypes/fileCommunication.js
@@ -29,6 +29,21 @@ export default class extends BaseObject {
     }
 }
 
+const DEFAULT_COMMUNICATION = {
+    '@type': 'DocumentFile',
+    objectType: 'file-cabinet-communication',
+    file: {
+        'file-cabinet-communication': {
+            abstract: '',
+            agent: {
+                givenName: '',
+                familyName: '',
+            },
+            dateCreated: '',
+        },
+    },
+};
+
 class CabinetFormElement extends BaseFormElement {
     static getAdditionalTypes = () => {
         return {
@@ -40,7 +55,7 @@ class CabinetFormElement extends BaseFormElement {
         console.log('-- Render CabinetFormElement --');
         console.log('render this.data', this.data);
 
-        let hit = getDocumentHit(this.data);
+        let hit = getDocumentHit(this._getData() ?? DEFAULT_COMMUNICATION);
         let communication = getCommunication(hit);
 
         // Schema:  https://gitlab.tugraz.at/dbp/middleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/communication.schema.json
@@ -51,30 +66,26 @@ class CabinetFormElement extends BaseFormElement {
                     subscribe="lang"
                     name="agent[givenName]"
                     label=${this._i18n.t('given-name')}
-                    .value=${communication.agent
-                        ? communication.agent.givenName
-                        : ''}></dbp-form-string-element>
+                    .value=${communication.agent.givenName}></dbp-form-string-element>
 
                 <dbp-form-string-element
                     subscribe="lang"
                     name="agent[familyName]"
                     label=${this._i18n.t('family-name')}
-                    .value=${communication.agent
-                        ? communication.agent.familyName
-                        : ''}></dbp-form-string-element>
+                    .value=${communication.agent.familyName}></dbp-form-string-element>
 
                 <dbp-form-string-element
                     subscribe="lang"
                     name="abstract"
                     label=${this._i18n.t('communication-abstract')}
                     rows="10"
-                    .value=${communication.abstract || ''}></dbp-form-string-element>
+                    .value=${communication.abstract}></dbp-form-string-element>
 
                 <dbp-form-datetime-element
                     subscribe="lang"
                     name="dateCreated"
                     label=${this._i18n.t('doc-modal-issue-date')}
-                    value=${communication.dateCreated || ''}
+                    value=${communication.dateCreated}
                     required></dbp-form-datetime-element>
 
                 ${this.getCommonFormElements()}

src/objectTypes/fileIdentityDocument.js
@@ -30,6 +30,18 @@ export default class extends BaseObject {
     }
 }
 
+const DEFAULT_IDENTITY_OCUMENT = {
+    '@type': 'DocumentFile',
+    objectType: 'file-cabinet-identityDocument',
+    file: {
+        'file-cabinet-identityDocument': {
+            nationality: '',
+            identifier: '',
+            dateCreated: '',
+        },
+    },
+};
+
 class CabinetFormElement extends BaseFormElement {
     static getAdditionalTypes() {
         return {
@@ -43,7 +55,7 @@ class CabinetFormElement extends BaseFormElement {
         console.log('-- Render CabinetFormElement --');
         console.log('render this.data', this.data);
 
-        let hit = getDocumentHit(this.data);
+        let hit = getDocumentHit(this._getData() ?? DEFAULT_IDENTITY_OCUMENT);
         let identityDocument = getIdentityDocument(hit);
 
         // Schema:  https://gitlab.tugraz.at/dbp/middleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/identityDocument.schema.json
@@ -54,7 +66,7 @@ class CabinetFormElement extends BaseFormElement {
                     subscribe="lang"
                     name="identifier"
                     label=${this._i18n.t('doc-modal-Identifier')}
-                    .value=${identityDocument.identifier || ''}
+                    .value=${identityDocument.identifier}
                     required></dbp-form-string-element>
 
                 <dbp-form-enum-element
@@ -62,14 +74,14 @@ class CabinetFormElement extends BaseFormElement {
                     name="nationality"
                     label=${this._i18n.t('doc-modal-nationality')}
                     .items=${formElements.getNationalityItems()}
-                    .value=${identityDocument.nationality || ''}
+                    .value=${identityDocument.nationality}
                     required></dbp-form-enum-element>
 
                 <dbp-form-date-element
                     subscribe="lang"
                     name="dateCreated"
                     label=${this._i18n.t('doc-modal-issue-date')}
-                    .value=${identityDocument.dateCreated || ''}
+                    .value=${identityDocument.dateCreated}
                     required></dbp-form-date-element>
 
                 ${this.getCommonFormElements()}

src/objectTypes/fileMinimalSchema.js
@@ -29,6 +29,16 @@ export default class extends BaseObject {
     }
 }
 
+const DEFAULT_MINIMAL_SCHEMA = {
+    '@type': 'DocumentFile',
+    objectType: 'file-cabinet-minimalSchema',
+    file: {
+        'file-cabinet-minimalSchema': {
+            dateCreated: null,
+        },
+    },
+};
+
 class CabinetFormElement extends BaseFormElement {
     static getAdditionalTypes() {
         return {
@@ -41,7 +51,7 @@ class CabinetFormElement extends BaseFormElement {
     render() {
         console.log('-- Render CabinetFormElement --');
         console.log('render this.data', this.data);
-        let hit = getDocumentHit(this.data);
+        let hit = getDocumentHit(this._getData() ?? DEFAULT_MINIMAL_SCHEMA);
         let minimalSchema = getMinimalSchema(hit);
 
         // Schema:  https://gitlab.tugraz.at/dbp/middleware/api/-/blob/main/config/packages/schemas/relay-blob-bundle/cabinet-bucket/minimalSchema.schema.json

commit a93b48a90883097b05234649e800715fec47cb9f

file upload: fix missing value for the additionType invisible form field
Regression from 9e9684f8ed36a614768bb2aa8cd1

additionType is special since it is set from outside and not part of
the default data.

src/baseObject.js
@@ -122,6 +122,7 @@ export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
     getCommonFormElements = () => {
         let hit = getDocumentHit(this._getData() ?? DEFAULT_FILE_COMMON);
         let fileCommon = hit.file.base;
+        const additionalType = this.additionalType || fileCommon.additionalType.key;
 
         return html`
             <dbp-form-string-element
@@ -162,7 +163,7 @@ export class BaseFormElement extends ScopedElementsMixin(DBPLitElement) {
                 rows="5"
                 .value=${fileCommon.comment || ''}></dbp-form-string-element>
 
-            <input type="hidden" name="additionalType" value="${fileCommon.additionalType.key}" />
+            <input type="hidden" name="additionalType" value="${additionalType}" />
             ${this.getButtonRowHtml()}
         `;
     };