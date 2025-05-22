1106c81218c193d90e351a90578118ce27687e71
Patrizio Bekerle <patrizio@bekerle.com>
2025-05-22 14:12:09 +0200

    toolkit: use shared nix and just config
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- shell.nix
+++ shell.nix
@@ -1,29 +1,18 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
+let
+  config = import ./vendor/toolkit/shared { inherit pkgs; };
+in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    nodejs_22
-    curl
-    zellij # smart terminal workspace
-    lazygit # git terminal
-    just # task runner
-    fzf # fuzzy finder, for "just watch"
-  ];
+  nativeBuildInputs =
+    config.nativeBuildInputs
+    ++ (with pkgs; [
+    ]);
 
-  shellHook = ''
-    export CHROMIUM_BIN=${pkgs.chromium}/bin/chromium
-    export FIREFOX_BIN=${pkgs.firefox}/bin/firefox
-
-    echo "Using chromium at $CHROMIUM_BIN and firefox at $FIREFOX_BIN for karma tests"
-
-    # Determine the repository root
-    REPO_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
-
-    # Check if we are in the repository root
-    if [ "$REPO_ROOT" = "$(pwd)" ]; then
-      # Symlink the pre-commit hook into the .git/hooks directory
-      ln -sf ../../scripts/pre-commit.sh .git/hooks/pre-commit
-    fi
-  '';
+  shellHook =
+    config.shellHook
+    + ''
+      echo "💻 Project dev shell"
+    '';
 }
