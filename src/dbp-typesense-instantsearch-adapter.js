6914ae7a8d19ddf06d75e0b9988468c4dfe0c460
Christoph Reiter <reiter.christoph@gmail.com>
2025-07-28 11:57:10 +0200

    typesense: remove group_by if there are no facets that need grouping
    In that case we can replace the groupby with a simple filter which removes
    everything except the primary documents of each group

--- src/dbp-typesense-instantsearch-adapter.js
+++ src/dbp-typesense-instantsearch-adapter.js
@@ -7,9 +7,8 @@ export default class DbpTypesenseInstantsearchAdapter extends TypesenseInstantSe
 
     facetConfigs = {};
 
-    // Set this to true to allow removing facet names from the request
-    allowFacetByFacetNameRemoval = true;
     removedFacetNames = ['person.person'];
+    facetsThatNeedGrouping = ['study.name', 'study.type', 'study.status.text'];
 
     /**
      * @param {CabinetFacets} facetComponent
@@ -22,21 +21,53 @@ export default class DbpTypesenseInstantsearchAdapter extends TypesenseInstantSe
         this.facetConfigs = facetConfigs;
     }
 
-    async _adaptAndPerformTypesenseRequest(instantsearchRequests) {
-        if (this.allowFacetByFacetNameRemoval) {
-            // Override originalFacetNames in case overrideFacetByData is enabled
-            for (const request of instantsearchRequests) {
-                let originalFacetNames = request.params.facets;
-                // facets can be undefined a string or an array
-                if (Array.isArray(originalFacetNames)) {
-                    originalFacetNames = originalFacetNames.filter(
-                        (item) => !this.removedFacetNames.includes(item),
-                    );
-                    request.params.facets = originalFacetNames;
+    _removeFacets(instantsearchRequests) {
+        // Remove facets for which we don't need any data
+        for (const request of instantsearchRequests) {
+            let originalFacetNames = request.params.facets;
+            // facets can be undefined a string or an array
+            if (Array.isArray(originalFacetNames)) {
+                originalFacetNames = originalFacetNames.filter(
+                    (item) => !this.removedFacetNames.includes(item),
+                );
+                request.params.facets = originalFacetNames;
+            }
+        }
+    }
+
+    _customGrouping(instantsearchRequests) {
+        // In case we only have facets that don't need grouping we can replace it
+        // with a simple filter_by on a per request basis. And in case none of them
+        // need grouping we can remove the group_by parameter completely.
+        let needGrouping = false;
+        for (const request of instantsearchRequests) {
+            const hasGroupingFacets = this.facetsThatNeedGrouping.some((item) =>
+                request.params.facets.includes(item),
+            );
+            if (!hasGroupingFacets) {
+                if (request.params.filters) {
+                    request.params.filters += ' && isPrimary:true';
+                } else {
+                    request.params.filters = 'isPrimary:true';
                 }
+            } else {
+                needGrouping = true;
             }
         }
 
+        // XXX: this is hacky, and depends on us re-initializing the adapter
+        // on facet changes. Ideally we would not change the global configuration
+        // but rather pass the grouping parameter to the request.
+        if (!needGrouping) {
+            this.configuration.additionalSearchParameters.group_by = undefined;
+            this.configuration.additionalSearchParameters.group_limit = undefined;
+            this.configuration.additionalSearchParameters.group_missing_values = undefined;
+        }
+    }
+
+    async _adaptAndPerformTypesenseRequest(instantsearchRequests) {
+        this._removeFacets(instantsearchRequests);
+        this._customGrouping(instantsearchRequests);
         return super._adaptAndPerformTypesenseRequest(instantsearchRequests);
     }
 }
