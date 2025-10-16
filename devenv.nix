3ab78a408bc5efb765d6c6fe93e89f709f4a6eb8
Patrizio Bekerle <patrizio@bekerle.com>
2025-10-16 11:02:43 +0200

    devenv: use shared i18next hook and remove legacy pre-commit file
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,26 +1,6 @@
 {
-  pkgs,
-  ...
-}:
-
-{
   # More config is provided by input shared
   enterShell = ''
     echo "🛠️ DBP Cabinet App Dev Shell"
   '';
-
-  # Pre-commit hooks
-  git-hooks.hooks = {
-    i18next = {
-      # Don't enable yet, since seems to run forever as git-hook
-      enable = false;
-      name = "i18next";
-      description = "Check translations with i18next-cli";
-      entry = "${pkgs.nodejs_24}/bin/npx i18next-cli extract --ci --dry-run";
-      language = "system";
-      pass_filenames = false;
-      files = "(src/.*\\.js$|translation\\.json$)";
-      stages = [ "pre-commit" ];
-    };
-  };
 }
