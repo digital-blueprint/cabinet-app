172daff1e99e7a77557ed99592cb094ac9d74c42
Patrizio Bekerle <patrizio@bekerle.com>
2026-02-16 15:20:29 +0100

    rollup: show when "dist" was built by "npm watch"
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- rollup.config.js
+++ rollup.config.js
@@ -472,6 +472,12 @@ Dependencies:
                       },
                   })
                 : false,
+            watch && {
+                name: 'log-rebuild',
+                writeBundle() {
+                    console.log(`\n[${new Date().toLocaleString()}] dist rebuilt\n`);
+                },
+            },
         ],
     };
 })();
