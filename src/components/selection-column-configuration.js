9f177905ba26d93fddc6df6a8556d064e5ee7d2c
Andrea Ortner <andrea.ortner@tugraz.at>
2026-04-22 13:22:38 +0200

    add aria-label to icons in column settings in multiaction dialog
    Signed-off-by: Andrea Ortner <andrea.ortner@tugraz.at>

--- src/components/selection-column-configuration.js
+++ src/components/selection-column-configuration.js
@@ -384,6 +384,13 @@ export class SelectionColumnConfiguration extends ScopedElementsMixin(DBPCabinet
             <dbp-icon-button
                 icon-name="${visible ? 'source_icons_eye-empty' : 'source_icons_eye-off'}"
                 class="column-visibility-icon"
+                aria-label="${visible
+                    ? this._i18n.t('selection-column-config.column-hide', {
+                          name: name,
+                      })
+                    : this._i18n.t('selection-column-config.column-show', {
+                          name: name,
+                      })}"
                 title="${visible
                     ? this._i18n.t('selection-column-config.column-hide', {
                           name: name,
