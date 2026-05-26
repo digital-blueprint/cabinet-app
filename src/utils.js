36e1238cd1a04d35045a564e1eddf2abca0d2620
Christoph Reiter <reiter.christoph@gmail.com>
2026-05-26 15:08:31 +0200

    Delete unused code

--- src/utils.js
+++ src/utils.js
@@ -57,12 +57,3 @@ export function formatDate(value) {
               year: 'numeric',
           });
 }
-
-export function debounce(func, delay) {
-    let timeout;
-    return function (...args) {
-        const context = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(context, args), delay);
-    };
-}
